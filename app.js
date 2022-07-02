a1d042c5324c5a005646695c68baf72dc9c96a18
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2022-07-02 18:19:40 -0300

    added dotenv

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var customersRouter = require("./routes/customers");
@@ -61,4 +62,7 @@ app.use(function (err, req, res, next) {
   res.json({ error: true, message: err.message });
 });
 
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Puerto conectado");
+});
 module.exports = app;
