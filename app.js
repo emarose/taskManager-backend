a7d546a54c471f796fd874d793c4e566cea8dc2b
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2022-07-02 19:56:19 -0300

    changes

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Puerto conectado");
+  console.log("Puerto conectado en el puerto " + process.env.PORT || 3000);
 });
 
 module.exports = app;
