8f418b9ef1ed29c2c48457862e57ecfc322b1d28
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2022-07-04 16:32:29 -0300

    rename file

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-//const homeController = require("../controllers/homeController");
-
-/* GET home page. */
-/* router.get("/", homeController.countAll); */
-
-module.exports = router;
