1c20d17f49cbd714d6d7233b9ad923df1c386925
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2022-07-04 17:57:35 -0300

    Update mongodb.js

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.connect(
-  "process.env.MONGODB_CONNECTION_STRING",
+  process.env.MONGODB_CONNECTION_STRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (error) {
     if (error) {
