0ce2746a514da60039133f24a0b177093b0a367b
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2022-07-04 17:51:38 -0300

    changes to URLs

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
-
+require("dotenv").config();
 mongoose.connect(
-  "mongodb+srv://emarose:12Metallica12@cluster0.wj35z.mongodb.net/rk2022?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
+  "process.env.MONGODB_CONNECTION_STRING",
+  { useNewUrlParser: true, useUnifiedTopology: true },
   function (error) {
     if (error) {
       throw error;
